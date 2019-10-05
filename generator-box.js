207e6f5147f6433cb9fc6e1f42b2d89ab5c461fd
Max <maxkoren@gmail.com>
2019-10-05 12:26:05 -0400

    Fixed generator box

--- generator-box.js
+++ generator-box.js
@@ -4,6 +4,8 @@ function GeneratorBox(node) {
 	let anchorY;
 	let countAcross;
 	let countDown;
+	let startY;
+	let startX;
 	
 	function getCount(pos, start, dim) {
 		return Math.max(Math.ceil(Math.abs(pos - start) / dim), 1);
@@ -11,45 +13,39 @@ function GeneratorBox(node) {
 	
 	this.start = function(e) {
 		measureScreen();
-		show(node);
 		node.style.zIndex = topZIndex;
 		anchorX = e.clientX;
 		anchorY = e.clientY;
 		this.move(e);
+		show(node);
 	};
 	
 	this.move = function(e) {
-		let x;
-		let y;
-		countAcross;
-		countDown;
+		let x = bound(e.clientX, 0, screenWidth  - TILE_WIDTH);
+		let y = bound(e.clientY, 0, screenHeight - TILE_HEIGHT);
 		
+		countAcross = getCount(x, gridFunc(anchorX), TILE_WIDTH);
+		countDown = getCount(y, gridFunc(anchorY), TILE_HEIGHT);
+
+		startX = gridFunc(anchorX);
+		startY = gridFunc(anchorY);
+
 		if (e.clientX < anchorX) {
-			// TODO
-		} else {
-			x = bound(e.clientX, 0, screenWidth  - TILE_WIDTH + 1);
-			countAcross = getCount(x, gridFunc(anchorX), TILE_WIDTH);
+			startX -= countAcross * TILE_WIDTH;
 		}
-		
 		if (e.clientY < anchorY) {
-			// TODO
-		} else {
-			y = bound(e.clientY, 0, screenHeight - TILE_HEIGHT + 1);
-			countDown = getCount(y, gridFunc(anchorY), TILE_HEIGHT);
+			startY -= countDown * TILE_HEIGHT;
 		}
 
+		node.style.transform = `translate(${startX}px, ${startY}px)`;
 		node.style.width =  `${countAcross * TILE_WIDTH }px`;
 		node.style.height = `${countDown   * TILE_HEIGHT}px`;
-		node.style.transform = `translate(${gridFunc(Math.min(x, anchorX))}px, ${gridFunc(Math.min(y, anchorY))}px)`;
 		node.innerText = countAcross * countDown;
 	};
 
 	this.stop = function(e) {
 		hide(node);
-		deck.placeGroup(
-				Math.min(bound(e.clientX, 0, screenWidth  - TILE_WIDTH),  anchorX), 
-				Math.min(bound(e.clientY, 0, screenHeight - TILE_HEIGHT), anchorY), 
-				countDown, countAcross * countDown, true);
+		deck.placeGroup(startX, startY, countDown, countAcross * countDown, true);
 	};
 	
 	this.cancel = function() {
